df9691c41b9e7ce51407996442976c1f5dc9b969
alextruesdale <alex.truesdale@colorado.edu>
2018-10-21 14:49:37 +0200

    Update to samsung_backup

--- bash-utility-scripts/samsung_backup/samsung_backup.sh
+++ bash-utility-scripts/samsung_backup/samsung_backup.sh
@@ -1,45 +1,53 @@
 #!/usr/bin/env bash
 
-echo "... syncing: data backups (brew, pip3, zsh, iterm, atom)"
+echo "... syncing: data backups (app list, brew, pip3, zsh, iterm, atom)"
 echo
 
 cd ~/Desktop/data/pip3
 pip3 list > pip3_info.txt
 pip3 freeze > pip3_installme.txt
+
 cd ../brew
 rm Brewfile
 brew bundle dump
 brew list > brew_installme.txt
 cat Brewfile >> Brewfile.txt
+
 cd ~/
-cp -r .oh-my-zsh /Users/alextruesdale/Desktop/data/zsh/
+rsync -av --exclude='.git/' .oh-my-zsh /Users/alextruesdale/Desktop/data/zsh/
 cp .zshrc /Users/alextruesdale/Desktop/data/zsh/
 cd /Users/alextruesdale/Desktop/data/zsh
+rm -r oh-my-zsh
 mv .oh-my-zsh oh-my-zsh
 mv .zshrc zshrc_config
+
 ls /Users/alextruesdale/.atom/packages > /Users/alextruesdale/Desktop/data/atom/package_list.txt
 cp -r /Users/alextruesdale/.atom/packages/solarised-neu /Users/alextruesdale/Desktop/data/atom/
 cp /Users/alextruesdale/.atom/styles.less /Users/alextruesdale/Desktop/data/atom/
 cd ~/
 
+find /Applications -maxdepth 1 -iname '*.app' > ~/Desktop/data/application_list.txt
+
+echo
+echo "update complete: data backups"
 echo "... syncing: downloads"
 echo
 
-rsync -av --delete-delay ~/Downloads/ /Volumes/SamsungT5/Dropbox/macbook_backup/Downloads/
+rsync -av --delete-delay ~/Downloads/ /Volumes/SamsungT5/Google\ Drive/macbook_backup/Downloads/
 
 echo
 echo "update complete: downloads"
 echo "... syncing: documents"
 echo
 
-rsync -av --delete-delay --exclude='/personal-site/' /Users/alextruesdale/Documents/ /Volumes/SamsungT5/Dropbox/macbook_backup/Documents/
+rsync -av --delete-delay --exclude-from='/Users/alextruesdale/Documents/personal-utility-scripts/bash-utility-scripts/samsung_backup/exclude_me.txt' /Users/alextruesdale/Documents/ /Volumes/SamsungT5/Google\ Drive/macbook_backup/Documents/
 
 echo
 echo "update complete: documents"
 echo "... syncing: desktop"
 echo
 
-rsync -av --delete-delay ~/Desktop/ /Volumes/SamsungT5/Dropbox/macbook_backup/Desktop/
+rsync -av --delete-delay ~/Desktop/ /Volumes/SamsungT5/Google\ Drive/macbook_backup/Desktop/
 
 echo
 echo "update complete: desktop"
@@ -47,8 +55,8 @@ echo "... syncing: library"
 echo
 
 cd ~/Library
-rsync -av Preferences /Volumes/SamsungT5/Dropbox/macbook_backup/Library/
-rsync -av Cookies /Volumes/SamsungT5/Dropbox/macbook_backup/Library/
+rsync -av Preferences /Volumes/SamsungT5/Google\ Drive/macbook_backup/Library/
+rsync -av Cookies /Volumes/SamsungT5/Google\ Drive/macbook_backup/Library/
 
 echo
 echo "update complete: library"
